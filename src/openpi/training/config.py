b54a7a85d5acf69bd8c88cd43e2662a007daee67
Sherry <sherry@chefrobotics.ai>
2025-12-24 20:15:18 -0800

    added lora config for chipotle scooping

--- src/openpi/training/config.py
+++ src/openpi/training/config.py
@@ -1440,16 +1440,16 @@ _CONFIGS = [
     ),
 
     TrainConfig(
-        name="pi0_chipotle-scoop",
+        name="pi0_chipotle-scoop-day1",
         model=pi0.Pi0Config(
             center_crop_square=True
             ),
         data=LeRobotAlohaDataConfig(
             use_delta_joint_actions=True, # default
             adapt_to_pi=False, # because Trossen v1.0 is different from standard Aloha data
-            repo_id="sandi/chipotle-scoop",
+            repo_id="sandi/chipotle-scoop-day1",
             base_config=DataConfig(
-                local_root="/home/inkyu/workspace/dataset/sandi/chipotle-scoop/",
+                local_root="/opt/data/sandi/chipotle-scoop-day1/",
                 prompt_from_task=True,
             ),
             default_prompt="Prepare a Chipotle-style bowl by scooping ingredients sequentially from left to right in the following order: rice, corn, beans, chicken, lettuce, and cheese. Use the scoop to transfer one ingredient at a time into the bowl. Fully complete one ingredient before moving to the next. Avoid spilling, keep the bowl centered, and place each ingredient neatly inside the bowl.",
@@ -1475,6 +1475,50 @@ _CONFIGS = [
         keep_period=10_000,
         batch_size=8,
         ema_decay=0.99,
+    ),
+    TrainConfig(
+        name="pi0_chipotle-scoop-day1-p1",
+        model=pi0.Pi0Config(
+            paligemma_variant="gemma_2b_lora", 
+            action_expert_variant="gemma_300m_lora", 
+            center_crop_square=True
+            ),
+        data=LeRobotAlohaDataConfig(
+            use_delta_joint_actions=True, # default
+            adapt_to_pi=False, # because Trossen v1.0 is different from standard Aloha data
+            repo_id="sandi/chipotle-scoop-day1-p1",
+            base_config=DataConfig(
+                local_root="/opt/data/sandi/chipotle-scoop-day1/",
+                prompt_from_task=True,
+            ),
+            default_prompt="Prepare a Chipotle-style bowl by scooping ingredients sequentially from left to right in the following order: rice, corn, beans, chicken, lettuce, and cheese. Use the scoop to transfer one ingredient at a time into the bowl. Fully complete one ingredient before moving to the next. Avoid spilling, keep the bowl centered, and place each ingredient neatly inside the bowl.",
+            repack_transforms=_transforms.Group(
+                inputs=[
+                    _transforms.RepackTransform(
+                        {
+                            "images": {
+                                "cam_high": "observation.images.cam_high",
+                                "cam_left_wrist": "observation.images.cam_left_wrist",
+                                "cam_right_wrist": "observation.images.cam_right_wrist",
+                                "cam_low": "observation.images.cam_low",
+                            },
+                            "state": "observation.state",
+                            "actions": "action",
+                        }
+                    )
+                ]
+            ),
+        ),
+        weight_loader=weight_loaders.CheckpointWeightLoader("gs://openpi-assets/checkpoints/pi0_base/params"),
+        freeze_filter=pi0.Pi0Config(
+            paligemma_variant="gemma_2b_lora", 
+            action_expert_variant="gemma_300m_lora", 
+            center_crop_square=True
+            ).get_freeze_filter(),
+        num_train_steps=30_000,
+        keep_period=10_000,
+        batch_size=8,
+        ema_decay=None,
     ),
 
     TrainConfig(
